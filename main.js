52af4b2f5b32d01e785360c20d35b93891eee6fa
Masamitsu Bando <bando@ktc.ac.jp>
2023-05-01 12:37:22 +0900

    始めだよ！

---
+++ main.js
@@ -0,0 +1,42 @@
+const { Player } = TextAliveApp;
+
+// 単語が発声されていたら #text に表示する
+const animateWord = function (now, unit) {
+  if (unit.contains(now)) {
+    document.querySelector("#text").textContent = unit.text;
+  }
+};
+
+// TextAlive Player を作る
+const player = new Player({
+  app: { token: "xxxxxxxxxx" },
+  mediaElement: document.querySelector("#media"),
+});
+
+player.addListener({
+  // 動画オブジェクトの準備が整ったとき（楽曲に関する情報を読み込み終わったとき）に呼ばれる
+  onVideoReady: (v) => {
+    // 定期的に呼ばれる各単語の "animate" 関数をセットする
+    let w = player.video.firstWord;
+    while (w) {
+      w.animate = animateWord;
+      w = w.next;
+    }
+  },
+  onAppReady: (app) => {
+    if (!app.managed) {
+      // king妃jack躍 / 宮守文学 feat. 初音ミク
+      player.createFromSongUrl("https://piapro.jp/t/ucgN/20230110005414", {
+        video: {
+          // 音楽地図訂正履歴: https://songle.jp/songs/2427948/history
+          beatId: 4267297,
+          chordId: 2405019,
+          repetitiveSegmentId: 2405019,
+          // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FucgN%2F20230110005414
+          lyricId: 56092,
+          lyricDiffId: 9636
+        },
+      });
+    }
+  }
+});
